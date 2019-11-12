1e416b2f49a7b8d3d8569d6b5fbd8d8ac0f22720
Steven Joruk <steven@joruk.com>
2019-11-12 12:39:23 +0000

    2018 4.1 WIP

--- rust/tests/2018.rs
+++ rust/tests/2018.rs
@@ -232,4 +232,72 @@ mod tests {
         assert_eq!(uncontested.len(), 1);
         assert_eq!(uncontested[0], 113);
     }
+
+    #[test]
+    fn test_day_4() {
+        #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+        enum EventType {
+            Begin(u16),
+            Sleep,
+            Wake,
+        }
+
+        #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+        struct Event {
+            year: u16,
+            month: u8,
+            day: u8,
+            hour: u8,
+            minute: u8,
+            event_type: EventType,
+        }
+
+        impl FromStr for Event {
+            type Err = ();
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let event_type: EventType;
+
+                if s[19..].find("falls asleep").is_some() {
+                    event_type = EventType::Sleep;
+                } else if s[19..].find("wakes up").is_some() {
+                    event_type = EventType::Wake;
+                } else {
+                    let sub = &s[s.find('#').expect("find #") + 1..];
+                    let id: u16 = sub[..sub.find(" ").expect("find id space")].parse().expect("parse id");
+                    event_type = EventType::Begin(id);
+                }
+
+                Ok(Event {
+                    year: s[1..5].parse().expect("year"),
+                    month: s[6..8].parse().expect("month"),
+                    day: s[9..11].parse().expect("day"),
+                    hour: s[12..14].parse().expect("hour"),
+                    minute: s[15..17].parse().expect("minute"),
+                    event_type,
+                })
+            }
+        }
+
+        let mut events: Vec<_> = load_input("2018-4")
+            .lines()
+            .map(|l| l.expect("unwrap line"))
+            .map(|l| Event::from_str(&l))
+            .map(|e| e.expect("unwrap event"))
+            .collect();
+
+        events.sort();
+
+        let mut totals = HashMap::new();
+
+        for event in events {
+            let mut current_id;
+            match event.event_type {
+                EventType::Begin(id) => current_id = id,
+                EventType::Begin => total.entry(current_id).or_insert(0) += 
+            }
+        }
+
+        assert_eq!(1, 2);
+    }
 }
