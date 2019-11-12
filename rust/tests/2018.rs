2e84eecedc87eb41426e63ad89bc96875d0afa37
Steven Joruk <steven@joruk.com>
2019-11-12 07:28:15 +0000

    2018 3.2

--- rust/tests/2018.rs
+++ rust/tests/2018.rs
@@ -137,7 +137,7 @@ mod tests {
     }
 
     #[test]
-    fn test_day_3_part_1() {
+    fn test_day_3() {
         #[derive(Eq, Hash, PartialEq)]
         struct Point {
             x: u16,
@@ -146,6 +146,7 @@ mod tests {
 
         #[derive(Debug)]
         struct Rect {
+            id: u16,
             x: u16,
             y: u16,
             w: u16,
@@ -159,6 +160,8 @@ mod tests {
 
         impl Fabric {
             fn add_claim(&mut self, r: &Rect) {
+                // Adding an rect.positions() iterator generator would be
+                // nicer.
                 for x in r.x..r.x + r.w {
                     for y in r.y..r.y + r.h {
                         let p = Point { x, y };
@@ -169,6 +172,18 @@ mod tests {
 
             fn get_contested_inches_count(&self) -> usize {
                 self.claims.values().filter(|v| **v > 1).count()
+            }
+
+            fn is_uncontested(&self, rect: &Rect) -> bool {
+                for x in rect.x..rect.x + rect.w {
+                    for y in rect.y..rect.y + rect.h {
+                        if self.claims[&Point { x, y }] != 1 {
+                            return false;
+                        }
+                    }
+                }
+
+                true
             }
         }
 
@@ -181,6 +196,7 @@ mod tests {
                 let dimensions: Vec<&str> = subs[3].split("x").collect();
 
                 let rect = Rect {
+                    id: subs[0][1..].parse().unwrap(),
                     x: points[0].parse().unwrap(),
                     // https://github.com/rust-lang/rfcs/issues/2249
                     y: points[1][..points[1].len() - 1].parse().unwrap(),
@@ -201,10 +217,19 @@ mod tests {
             .map(|r| r.unwrap())
             .collect();
 
-        for rect in rects {
-            fabric.add_claim(&rect);
+        for rect in &rects {
+            fabric.add_claim(rect);
         }
 
         assert_eq!(fabric.get_contested_inches_count(), 111_485);
+
+        let uncontested: Vec<_> = rects
+            .iter()
+            .filter(|r| fabric.is_uncontested(r))
+            .map(|r| r.id)
+            .collect();
+
+        assert_eq!(uncontested.len(), 1);
+        assert_eq!(uncontested[0], 113);
     }
 }
