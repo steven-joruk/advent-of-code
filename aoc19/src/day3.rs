35c71bd0e9acf14af285c64668e09e5b89add9db
Steven Joruk <steven@joruk.com>
2019-12-03 20:42:02 +0000

    2019 3.2

--- aoc19/src/day3.rs
+++ aoc19/src/day3.rs
@@ -1,30 +1,31 @@
 static INPUT: &str = include_str!("../res/3");
 
-pub fn solve() {
-    part1();
-    part2();
-}
-
 // TODO: See if I can combine Move/Line in one pass during parsing
 
-#[derive(Debug)]
-enum Move {
-    Up(i16),
-    Down(i16),
-    Left(i16),
-    Right(i16),
+enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+struct Move {
+    dir: Dir,
+    length: i16,
 }
 
 impl From<&str> for Move {
     fn from(input: &str) -> Self {
-        let distance = input[1..].parse::<i16>().unwrap();
-        match input.chars().next() {
-            Some('U') => Move::Up(distance),
-            Some('D') => Move::Down(distance),
-            Some('L') => Move::Left(distance),
-            Some('R') => Move::Right(distance),
+        let length = input[1..].parse::<i16>().unwrap();
+        let dir = match input.chars().next() {
+            Some('U') => Dir::Up,
+            Some('D') => Dir::Down,
+            Some('L') => Dir::Left,
+            Some('R') => Dir::Right,
             ch => panic!("Unsupported direction: {:?}", ch),
-        }
+        };
+
+        Move { dir, length }
     }
 }
 
@@ -46,7 +47,7 @@ struct Line {
 }
 
 impl Line {
-    fn intersection(&self, other: &Line) -> Option<Point> {
+    fn point_intersecting(&self, other: &Line) -> Option<Point> {
         if !self.is_perpendicular_to(other) {
             return None;
         }
@@ -86,6 +87,38 @@ impl Line {
         Some(point)
     }
 
+    fn steps_to_intersect(&self, point: &Point) -> Option<i16> {
+        if self.is_vertical() {
+            if self.from.x != point.x {
+                return None;
+            }
+
+            if self.from.y < point.y && self.to.y < point.y {
+                return None;
+            }
+
+            if self.from.y > point.y && self.to.y > point.y {
+                return None;
+            }
+
+            return Some((self.from.y - point.y).abs());
+        }
+
+        if self.from.y != point.y {
+            return None;
+        }
+
+        if self.from.x < point.x && self.to.x < point.x {
+            return None;
+        }
+
+        if self.from.x > point.x && self.to.x > point.x {
+            return None;
+        }
+
+        Some((self.from.x - point.x).abs())
+    }
+
     #[inline]
     fn is_perpendicular_to(&self, other: &Line) -> bool {
         self.is_vertical() != other.is_vertical()
@@ -93,7 +126,16 @@ impl Line {
 
     #[inline]
     fn is_vertical(&self) -> bool {
-        self.from.y != self.to.y
+        self.from.x == self.to.x
+    }
+
+    #[inline]
+    fn length(&self) -> i16 {
+        if self.is_vertical() {
+            self.from.y.max(self.to.y) - self.from.y.min(self.to.y)
+        } else {
+            self.from.x.max(self.to.x) - self.from.x.min(self.to.x)
+        }
     }
 }
 
@@ -107,11 +149,11 @@ fn create_lines_from_moves(moves: &[Move]) -> Vec<Line> {
     for mv in moves {
         position = last_position;
 
-        match *mv {
-            Move::Up(d) => position.y -= d,
-            Move::Down(d) => position.y += d,
-            Move::Left(d) => position.x -= d,
-            Move::Right(d) => position.x += d,
+        match mv.dir {
+            Dir::Up => position.y -= mv.length,
+            Dir::Down => position.y += mv.length,
+            Dir::Left => position.x -= mv.length,
+            Dir::Right => position.x += mv.length,
         }
 
         lines.push(Line {
@@ -129,7 +171,7 @@ fn find_path_intersections(wire1: &[Line], wire2: &[Line]) -> Vec<Point> {
     let mut intersections = Vec::new();
     for line1 in wire1 {
         for line2 in wire2 {
-            if let Some(point) = line1.intersection(line2) {
+            if let Some(point) = line1.point_intersecting(line2) {
                 intersections.push(point);
             }
         }
@@ -138,7 +180,21 @@ fn find_path_intersections(wire1: &[Line], wire2: &[Line]) -> Vec<Point> {
     intersections
 }
 
-fn part1() {
+fn count_steps_to_intersection(path: &[Line], point: &Point) -> i16 {
+    let mut total: i16 = 0;
+
+    for line in path {
+        if let Some(steps) = line.steps_to_intersect(point) {
+            return total + steps;
+        }
+
+        total += line.length();
+    }
+
+    unreachable!();
+}
+
+pub fn solve() {
     let wires_moves: Vec<Vec<Move>> = INPUT
         .lines()
         .map(|l| l.split(',').map(|m| Move::from(m)).collect())
@@ -147,12 +203,18 @@ fn part1() {
     let wire1_path = create_lines_from_moves(&wires_moves[0]);
     let wire2_path = create_lines_from_moves(&wires_moves[1]);
 
-    let min = find_path_intersections(&wire1_path, &wire2_path)
-        .iter()
-        .map(|p| p.manhattan_distance())
-        .min();
-
+    let intersections = find_path_intersections(&wire1_path, &wire2_path);
+    let min = intersections.iter().map(|p| p.manhattan_distance()).min();
     println!("The manhattan distance is {:?}", min);
-}
 
-fn part2() {}
+    let mut steps = Vec::new();
+
+    for intersection in intersections {
+        let wire1_steps = count_steps_to_intersection(&wire1_path, &intersection);
+        let wire2_steps = count_steps_to_intersection(&wire2_path, &intersection);
+        steps.push(wire1_steps + wire2_steps);
+    }
+
+    let min = steps.iter().min();
+    println!("Minimum steps required: {:?}", min);
+}
