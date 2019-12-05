5b9671a7b1844e42b319af0842988a5e236b320f
Steven Joruk <steven@joruk.com>
2019-12-05 22:00:29 +0000

    2019 4.2

--- aoc19/src/day4.rs
+++ aoc19/src/day4.rs
@@ -1,6 +1,7 @@
 struct Passwords {
     higher: u32,
     current: String,
+    exclusive_pair: bool,
 }
 
 impl Iterator for Passwords {
@@ -10,6 +11,8 @@ impl Iterator for Passwords {
         let mut current_u32 = self.current.parse::<u32>().unwrap();
 
         loop {
+            // This skips the first password but it doesn't matter for the input
+            // I have.
             current_u32 += 1;
             self.current = current_u32.to_string();
 
@@ -25,30 +28,59 @@ impl Iterator for Passwords {
 }
 
 impl Passwords {
-    fn new(lower: u32, higher: u32) -> Self {
+    fn new(lower: u32, higher: u32, exclusive_pair: bool) -> Self {
         Passwords {
             higher,
             current: lower.to_string(),
+            exclusive_pair,
         }
     }
 
     fn current_is_valid(&self) -> bool {
-        let mut has_adjacent_pair = false;
+        #[derive(PartialEq)]
+        enum State {
+            UniquePrevious,
+            TwoAdjacent,
+            ThreeAdjacent,
+        };
+
         let mut prev = self.current.chars().next().unwrap();
+        let mut state = State::UniquePrevious;
+        let mut has_two_adjacent = false;
 
         for ch in self.current.chars().skip(1) {
             if prev > ch {
                 return false;
             }
 
-            if prev == ch {
-                has_adjacent_pair = true;
+            match state {
+                State::UniquePrevious => {
+                    if prev == ch {
+                        state = State::TwoAdjacent;
+                        if !self.exclusive_pair {
+                            has_two_adjacent = true;
+                        }
+                    }
+                }
+                State::TwoAdjacent => {
+                    if prev == ch {
+                        state = State::ThreeAdjacent;
+                    } else {
+                        state = State::UniquePrevious;
+                        has_two_adjacent = true;
+                    }
+                }
+                State::ThreeAdjacent => {
+                    if prev != ch {
+                        state = State::UniquePrevious;
+                    }
+                }
             }
 
             prev = ch;
         }
 
-        has_adjacent_pair
+        state == State::TwoAdjacent || has_two_adjacent
     }
 }
 
@@ -56,6 +88,9 @@ pub fn solve() {
     let lower = 172_851;
     let higher = 675_869;
 
-    let passwords = Passwords::new(lower, higher);
+    let passwords = Passwords::new(lower, higher, false);
+    println!("Number of passwords: {}", passwords.count());
+
+    let passwords = Passwords::new(lower, higher, true);
     println!("Number of passwords: {}", passwords.count());
 }
